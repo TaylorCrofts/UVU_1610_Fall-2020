b4a7575fa38e2b61d98def156310029a66babb61
Taylor Crofts <10873957@my.uvu.edu>
2020-09-14 14:53:21 -0600

    Replaced Playercontroller with functioning PlayerController
    Ended up re doing the script in jetbrains to understand the problem better and reviewing the lesson on unity to get it right.

---
+++ Create with Code/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed= 12;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Is moving");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward *Time.deltaTime*speed);
+        //move the vehicle 
+    }
+}
