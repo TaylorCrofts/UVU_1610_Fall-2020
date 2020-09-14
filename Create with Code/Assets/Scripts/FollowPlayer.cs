961cdbaf1681517c0f16a9849817086472d9f676
Taylor Crofts <10873957@my.uvu.edu>
2020-09-14 15:30:25 -0600

    FollowPlayer camera script added
    A script which can be assigned the game object of my choice to follow according to it's transform functions

---
+++ Create with Code/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 6, -12);
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position +offset;
+    }
+}
--- Create with Code/Assets/Scripts/PlayerController.cs
+++ Create with Code/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed= 20;
+    public float speed= 20.1f;
     
     // Start is called before the first frame update
     void Start()
