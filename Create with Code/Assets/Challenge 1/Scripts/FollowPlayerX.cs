f872a788cf7a9489c053e960e1f235a6a050500d
Taylor Crofts <10873957@my.uvu.edu>
2020-09-15 22:19:32 -0600

    Made plane forward. Control plane limited. and camera follows
    The speed is reduced, the camera can follow player from side view, the plane moves forward but the up makes it go down and the down makes it go up so I have to fix that.

--- Create with Code/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Create with Code/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -1,19 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    private Vector3 offset= new Vector3(-18,3,5);
     void Update()
     {
         transform.position = plane.transform.position + offset;
--- Create with Code/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Create with Code/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 0.45f;
     public float rotationSpeed;
     public float verticalInput;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -21,9 +13,9 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
     }
 }
