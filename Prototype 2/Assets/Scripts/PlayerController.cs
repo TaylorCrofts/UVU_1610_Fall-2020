c62aaa989889c3688eb8f6d7848eeac0571b8a8b
Taylor Crofts <10873957@my.uvu.edu>
2020-09-21 17:10:22 -0600

    Prototype 2 downloaded and set up
    just basic set up to begin working

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{ 
+    //access type + The data type + the name + value input
+    public float horizantalimput;
+
+    
+    void Update()
+    {
+        horizantalimput = Input.GetAxis("Horizontal");
+        
+    }
+}
