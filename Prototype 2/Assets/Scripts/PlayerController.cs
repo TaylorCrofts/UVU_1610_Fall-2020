81e5e907e588aaf989394e7ce8d3e899e6ecdc54
Taylor Crofts <10873957@my.uvu.edu>
2020-09-24 22:21:42 -0600

    Fixed clean up mistake
    accidentally caused an error through removal of transform from playercontroller script

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -1,30 +1,27 @@
 ﻿using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     private float speed = 25.1f;
     private float horizontalInput;
-    public float xRange = 10.00f;
+    public float xRange = 19.5f;
     void Start()
     {
-        print("player moves, left and right to a limited axiss according to playercontroller script test");
+        print("player moves, left and right to a limited axis according to playercontroller script test");
     }
-
-    // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-
         //Turn the Farmer side to side 
         transform.Translate(Vector3.right*Time.deltaTime*speed*horizontalInput);
        
         if (transform.position.x < -xRange)
         {
-            transform.position =new Vector3(-xRange, 0,0);
+            transform.position =new Vector3(-xRange, 0,transform.position.z);
         }
         if (transform.position.x > xRange)
         { 
-            transform.position =new Vector3(xRange, 0,0);
+            transform.position =new Vector3(xRange, 0,transform.position.z); 
         }
-    }
+    } 
 }
+//For some reason I have to define transform position of z or else the player will move and stick to the other side of the screen
