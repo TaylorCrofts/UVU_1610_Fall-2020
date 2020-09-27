a6a65af84774c101c5b1724d9903ac335ea78c71
Taylor Crofts <10873957@my.uvu.edu>
2020-09-26 21:18:41 -0600

    Added instantiate to playercontroller to call prefab

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -2,27 +2,33 @@
 public class PlayerController : MonoBehaviour
 {
     private float speed = 25.1f;
-    private float horizontalInput;
+    float horizontalInput;
     public float xRange = 19.5f;
+    
     public GameObject projectilePrefab;
     void Start()
     {
         print("player moves, left and right to a limited axis according to playercontroller script test");
+        print("player instantiates prefab (projectilePrefab) into scene) ");
     }
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        //Turn the Farmer side to side 
         transform.Translate(Vector3.right*Time.deltaTime*speed*horizontalInput);
-       
+
         if (transform.position.x < -xRange)
         {
-            transform.position =new Vector3(-xRange, 0,transform.position.z);
+            transform.position =new Vector3(-xRange, transform.position.y,transform.position.z);
         }
         if (transform.position.x > xRange)
         { 
-            transform.position =new Vector3(xRange, 0,transform.position.z); 
+            transform.position =new Vector3(xRange, transform.position.y,transform.position.z); 
         }
-    } 
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
 }
 //For some reason I have to define transform position of z or else the player will move and stick to the other side of the screen
