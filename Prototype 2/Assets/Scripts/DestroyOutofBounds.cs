626e913bfda417863e550ba1ab30bc807e813456
Taylor Crofts <10873957@my.uvu.edu>
2020-09-28 17:20:41 -0600

    Added out of bounds to destroy prefabs once off screen

---
+++ Prototype 2/Assets/Scripts/DestroyOutofBounds.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DestroyOutofBounds : MonoBehaviour
+{
+    public float topBounds = 30f;
+    public float lowerBounds= -10f;
+    
+    void Update()
+    {
+        if (transform.position.z>topBounds)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBounds)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
