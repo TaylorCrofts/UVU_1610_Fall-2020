66b83338a7fc5410c316f376033521234bbb7a48
Taylor Crofts <10873957@my.uvu.edu>
2020-09-30 17:30:50 -0600

    Spawn and randomize system
    Added spawnmanager (empty GO) and SpawnManager (Script) which spawns random game objects at random positions.

--- Prototype 2/Assets/Scripts/DestroyOutofBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutofBounds.cs
@@ -4,16 +4,17 @@ public class DestroyOutofBounds : MonoBehaviour
 {
     public float topBounds = 30f;
     public float lowerBounds= -10f;
-    
     void Update()
     {
         if (transform.position.z>topBounds)
         {
             Destroy(gameObject);
+            //Will destroy object exiting the topbounds of camera view
         }
         else if (transform.position.z < lowerBounds)
         {
             Destroy(gameObject);
+            //will destroy object exiting lowerbounds of camera view
         }
     }
 }
---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{ //array holds multiples of values
+    //0 is the first in a sequence when tha placeholder is being followed by an array.
+   
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
+    public float spawnRangeX =20f;
+    public float spawnPossZ =20f;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //randomly generate animals at random transform.positions
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0, spawnPossZ);
+            
+            //("prefab".Length gives you the max that is assigned and available to be called)
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            
+            //debug.log (takes variable) gives you its call (Number, name)
+            Debug.Log(animalIndex);
+           
+            //instantiate has 3 parts. The thing you want to bring into the scene (gameobject/prefab, the spawn position of the object, and the rotation of the object)
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
