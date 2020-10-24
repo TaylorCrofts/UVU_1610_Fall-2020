0ca2fdb56039fc0e9f78c150d81d6e99068e1a21
Taylor Crofts <10873957@my.uvu.edu>
2020-10-23 20:01:32 -0600

    Added Player controller (Ptype3)

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRB;
+    
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        playerRB.AddForce(Vector3.up * 100);
+
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
