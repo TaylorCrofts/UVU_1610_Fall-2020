1f32001d4f0c346a6965773aff52a854c32d66d0
Taylor Crofts <10873957@my.uvu.edu>
2020-10-29 19:29:41 -0600

    Created Spawn Manager

---
+++ Prototype 3/Assets/Scripts/SpawnManagement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagement : MonoBehaviour
+{
+    public GameObject gamePrefab;
+    private Vector3 sponPos = new Vector3(25,0,-1);
+    void Start()
+    {
+        Instantiate(gamePrefab, sponPos, gamePrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
