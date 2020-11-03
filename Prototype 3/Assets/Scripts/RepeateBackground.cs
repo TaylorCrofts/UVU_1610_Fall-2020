138b812bb3bc6262448aa2e9608636fa3d984605
Taylor Crofts <10873957@my.uvu.edu>
2020-11-02 22:59:07 -0700

    RepeatBackground  Script added to background

---
+++ Prototype 3/Assets/Scripts/RepeateBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeateBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    
+    void Update()
+    {
+        if (transform.position.x <startPos.x -50)
+        {
+            transform.position = startPos;
+        }
+    }
+}
