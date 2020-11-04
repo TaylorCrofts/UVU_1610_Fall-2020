4e348e2ffd0b6a97c105570f0bc4628e814ef5aa
Taylor Crofts <10873957@my.uvu.edu>
2020-11-03 22:49:48 -0700

    Death animation triggered on gameover

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -39,6 +39,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
