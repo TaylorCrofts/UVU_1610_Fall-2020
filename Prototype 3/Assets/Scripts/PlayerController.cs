d5d2cc704c4b811fdcee298f85c73e33f98fc376
Taylor Crofts <10873957@my.uvu.edu>
2020-11-04 01:51:30 -0700

    Final sound effects working. Mod 3 Complete

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private AudioSource playerAudio;
     public float jumpModifier = 10f;
     public float gravityModifier = 1f;
     public bool isOnGround = true;
     public bool gameOver;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticles;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     private Rigidbody playerRB;
     private Animator playerAnim;
     
@@ -20,6 +23,7 @@ public class PlayerController : MonoBehaviour
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
         dirtParticles.Play();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -29,6 +33,8 @@ public class PlayerController : MonoBehaviour
             playerRB.AddForce(Vector3.up*jumpModifier,ForceMode.Impulse);
             playerAnim.SetTrigger("Jump_trig");
             isOnGround = false;
+            dirtParticles.Stop();
+            playerAudio.PlayOneShot(jumpSound,1f);
         }
     }
 
@@ -42,10 +48,12 @@ public class PlayerController : MonoBehaviour
         }else if (collision.gameObject.CompareTag("Obstical"))
         {
             gameOver = true;
-            dirtParticles.Stop();
+            
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            playerAudio.PlayOneShot(crashSound,1f);
+            dirtParticles.Stop();
             explosionParticle.Play();
             
         }
