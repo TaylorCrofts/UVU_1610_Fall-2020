c0e7b7378f8d6ce8f7547305a18b53e080961d68
Taylor Crofts <10873957@my.uvu.edu>
2020-11-06 01:38:55 -0700

    Lab 3 character movement plan

---
+++ Personal Project/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //On start I want the game to auto apply a starting forward momentum.
+        //and this will trigger a starting animation "wiggle"
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //I want players to be able to "jump" and "wreak" 
+        //But since the point is to keep going the player doesn't need to control
+        //forward or backward momentum they will gain speed as they go.
+    }
+}
+//player will need animations tied to speed, health, and ability use
+//Ability "bash" is a downward applied force that that is equal to half the speed 
+//player is making. obsticals that take enough force will be destroyed 
+//obsticals that are destroid by the player obsorb the momentum and so the player is slowed
+
+
