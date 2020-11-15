18e15727623c0ba0ee923459a78706810c6bed9b
Taylor Crofts <10873957@my.uvu.edu>
2020-11-15 00:21:33 -0700

    PowerUp Update
    PowerUp Prefab created; PUP is destroyed upon collision; PlayerController script is able to push enemy away with force so long as they have collided with PUP

--- ProtoType 4/Assets/Scripts/PlayerController.cs
+++ ProtoType 4/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed;
     private GameObject focalPoint;
+    public bool hasPowerUp;
+    private float powerUpStrength = 15.0f;
     
     void Start()
     {
@@ -16,5 +18,26 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")&& hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            
+            Debug.Log(("Player collided with" +collision.gameObject+ "with PowerUp set to" + hasPowerUp));
+        }
     }
 }
