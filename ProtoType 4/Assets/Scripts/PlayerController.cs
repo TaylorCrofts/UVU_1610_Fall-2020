319d107981fe89d9e53ffc6d4e0f2577b43026c3
Taylor Crofts <10873957@my.uvu.edu>
2020-11-14 22:49:49 -0700

    script cleaning

--- ProtoType 4/Assets/Scripts/PlayerController.cs
+++ ProtoType 4/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,15 +6,12 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private GameObject focalPoint;
     
-    
-    // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("FocalPoint");
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
