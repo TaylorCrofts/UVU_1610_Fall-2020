049d77eb7a84c04b5a88048a7ca00b2b910e27c7
Taylor Crofts <10873957@my.uvu.edu>
2020-11-14 22:32:37 -0700

    Focal point and camera rotation control added
    added focal point to the center of island (Empty gameObject to remain invisible. Made main camera child to focalpoint. Created rotate camera script so that the camera rotates around the focal point according to Horizontal input.

---
+++ ProtoType 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+    public class RotateCamera : MonoBehaviour
+    {
+        public float rotationSpeed = 5f;
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up, horizontalInput * rotationSpeed* Time.deltaTime);
+        }
+    }
