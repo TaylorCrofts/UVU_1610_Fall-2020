a9471f1d81fe95df5f9a693ad03c1886d40ebdfc
Taylor Crofts <10873957@my.uvu.edu>
2020-11-24 19:21:54 -0700

    Enemy now destroyed on collision with goal
    that triggers a respawn. Respawn rate is not working yet

--- ProtoType 4/Assets/Challenge 4/Scripts/EnemyX.cs
+++ ProtoType 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -13,23 +13,19 @@ public class EnemyX : MonoBehaviour
         playerGoal = GameObject.Find("Player Goal");
         enemyRb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        // Set enemy direction towards player goal and move there. The computer should determine that the space the ball needs to move
-        //is it's own position - the position of the goal.
+      
         Vector3 ObjectiveDirection = (playerGoal.transform.position - transform.position).normalized;
         
-        //This should add force to enemy to get it to move.
         enemyRb.AddForce(ObjectiveDirection * speed);
 
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        // If enemy collides with either goal, destroy it
-        if (other.gameObject.name == "Enemy Goal" && other.gameObject.name == "Player Goal")
+        if (other.gameObject.name == "Player Goal")
         {
             Destroy(gameObject);
             Debug.Log("GOAL!");
