7ce39255f71eed9e8e9a1f8d164b3c61a5d6944c
Taylor Crofts <10873957@my.uvu.edu>
2020-12-12 16:27:26 -0700

    Added Target Script to Prefabs
    Target script now had Random.range. Making it so objects are thrown up and rotate randomly

---
+++ Prototype5/Assets/Scripts/Target.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    
+    private void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(Vector3.up * Random.Range(12, 18), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10,10), Random.Range(-10,10), Random.Range(-10,10), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4,4), -6);
+    }
+    
+    // private void Update()
+    // {
+    //     
+    // }
+}
