54b25deb73c3f660b16db193873aac7f82e72a8b
Taylor Crofts <10873957@my.uvu.edu>
2020-12-12 19:47:54 -0700

    Added force, torque, and spawnpos (Target script)
    force is applied to the GO, which makes the object shoot up, Torque applies a spin to give it life, SpawnPos dictates where it can spawn off screen.

--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private float maxSpeed = 12f;
+    private float minSpeed = 18f;
+    private float torques = 10f;
+    private float xRangeX = 4f;
+    private float ySponPos = -6f;
+    
     
     private void Start()
     {
-        targetRb = GetComponent<Rigidbody>();
-        targetRb.AddForce(Vector3.up * Random.Range(12, 18), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10,10), Random.Range(-10,10), Random.Range(-10,10), ForceMode.Impulse);
-        transform.position = new Vector3(Random.Range(-4,4), -6);
+        targetRb = GetComponent<Rigidbody>(); //Tells the game to check for rigidbody.
+        
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse); //See RandomForce method.
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);//see RandomTorque method.
+        transform.position = (RandomSpawnPos()); //see RandomSpawnPos function.
     }
     
     // private void Update()
     // {
     //     
     // }
+
+    Vector3 RandomForce() //Adds a random amount of force upward as GO's instantiate onto scene.
+    {
+        return Vector3.up * Random.Range(maxSpeed, minSpeed);
+    }
+
+    float RandomTorque() //Adds a random spin to the GO's as they move in game.
+    {
+        return Random.Range(-torques,torques);
+    }
+
+    Vector3 RandomSpawnPos() //Creates a random spawn position.
+    {
+        return new Vector3(Random.Range(-xRangeX,xRangeX),xRangeX);
+    }
+    
 }
