99146b9a2127da43138eee002e22fe8e9057461a
Taylor Crofts <10873957@my.uvu.edu>
2020-12-12 20:10:24 -0700

    GameManager now Spawns GO's on play

--- Prototype5/Assets/Scripts/GameManager.cs
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject> Targets;
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
 
-    // void Start()
-    // {
-    //     
-    // }
-    //
-    //
-    // void Update()
-    // {
-    //     
-    // }
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
 }
