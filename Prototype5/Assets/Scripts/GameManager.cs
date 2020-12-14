43d3bb33a9802fa161e33e246b9f8821e7dc5667
Taylor Crofts <10873957@my.uvu.edu>
2020-12-14 00:27:33 -0700

    Added Score Variable to Game manager script

--- Prototype5/Assets/Scripts/GameManager.cs
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    private int Score;
+    public TextMeshProUGUI scoreText;
+    
     
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        Score = 0;
+        scoreText.text = "Score: " + Score;
     }
 
     IEnumerator SpawnTarget()
